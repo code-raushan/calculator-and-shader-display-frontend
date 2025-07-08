990dcaef0dd27d2298abaf591fb16037389aa780
Raushan Kumar <raushan.development@gmail.com>
2025-07-08 20:28:05 +0530

    feat: integrated rust wasm calculator methods in UI

--- calculator/src/lib.rs
+++ calculator/src/lib.rs
@@ -32,16 +32,85 @@ impl Calculator {
 
     pub fn handle_button(&mut self, key: &str) {
         match key {
-            "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => {
-                println!("Number: {}", key);
-            }
+            "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => self.handle_number(key),
             "C" => self.handle_clear(),
-            "." => println!("Decimal: {}", key),
-            "+" | "-" | "*" | "/" => println!("Operator: {}", key),
-            "=" => println!("Equals: {}", key),
-            "+/-" => println!("Sign: {}", key),
-            "%" => println!("Percent: {}", key),
+            "." => self.handle_decimal(),
+            "+" | "-" | "*" | "/" => self.handle_operator(key.chars().next().unwrap()),
+            "=" => self.handle_equals(),
+            "+/-" => self.handle_sign_change(),
+            "%" => self.handle_percentage(),
             _ => println!("Unknown key: {}", key),
+        }
+    }
+
+    pub fn handle_number(&mut self, num: &str) {
+        if self.expecting_new_input || self.current_input == "0" {
+            self.current_input = num.to_string();
+            self.expecting_new_input = false;
+        } else {
+            self.current_input.push_str(num);
+        }
+    }
+
+    pub fn handle_decimal(&mut self) {
+        if self.expecting_new_input {
+            self.current_input = "0.".to_string();
+            self.expecting_new_input = false;
+        } else if !self.current_input.contains(".") {
+            self.current_input.push('.');
+        }
+    }
+
+    pub fn handle_operator(&mut self, op: char) {
+        if self.expecting_new_input && self.operator.is_some() {
+            self.operator = Some(op);
+            return;
+        }
+
+        if self.operator.is_some(){
+            self.handle_equals();
+        }
+
+        self.previous_value = self.current_input.parse().unwrap_or(0.0);
+        self.operator = Some(op);
+        self.expecting_new_input = true;
+    }
+
+    pub fn handle_equals(&mut self) {
+        if let Some(op) = self.operator {
+            let current_value = self.current_input.parse().unwrap_or(0.0);
+            let result = match op
+             {
+                '+' => self.previous_value + current_value,
+                '-' => self.previous_value - current_value,
+                '*' => self.previous_value * current_value,
+                '/' => {
+                    if current_value != 0.0 {
+                        self.previous_value / current_value
+                    } else {
+                        0.0
+                    }
+                },
+                _ => current_value,
+            };
+
+            self.current_input = result.to_string();
+            self.previous_value = result;
+            self.operator = None;
+            self.expecting_new_input = true;
+        }
+    }
+
+    pub fn handle_percentage(&mut self) {
+        let value = self.current_input.parse().unwrap_or(0.0);
+        self.current_input = (value / 100.0).to_string();
+        self.expecting_new_input = true;
+    }
+
+    fn handle_sign_change(&mut self) {
+        let value = self.current_input.parse().unwrap_or(0.0);
+        if value != 0.0 {
+            self.current_input = (value * -1.0).to_string();
         }
     }
 }
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,11 +1,29 @@
+import init, { Calculator as CalculatorWasm } from "calculator";
+import { useEffect, useRef, useState } from "react";
 import Display from "./calculator/Display";
 import Keypad from "./calculator/Keypad";
 
 const Calculator = () => {
+  const [displayValue, setDisplayValue] = useState<string>("0");
+  const calculator = useRef<CalculatorWasm | null>(null);
+
+  useEffect(() => {
+    init().then(() => {
+      calculator.current = CalculatorWasm.new();
+      setDisplayValue(calculator.current.get_current_display());
+    });
+  }, []);
+
+  const handleButtonClick = (value: string) => {
+    if (calculator.current) {
+      calculator.current.handle_button(value);
+      setDisplayValue(calculator.current.get_current_display());
+    }
+  };
   return (
     <div className="w-full max-w-xs mx-auto mt-10 bg-gray-900 rounded-lg shadow-lg p-4">
-      <Display value="0" />
-      <Keypad onButtonClick={() => {}} />
+      <Display value={displayValue} />
+      <Keypad onButtonClick={handleButtonClick} />
     </div>
   );
 };
